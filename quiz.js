a3494aafa2ac78ae9d831c54152c651498ba258e
yahiaomarab <yahiaomar746@gmail.com>
2025-01-19 16:18:05 +0200

    task 0 done

---
+++ quiz.js
@@ -0,0 +1,18 @@
+function checkAnswer() {
+  const radioButton = document.querySelectorAll('[name="quiz"]');
+  const textStatus = document.getElementById("feedback");
+  let userAnswer;
+  let correctAnswer = 4;
+  radioButton.forEach((answer) => {
+    if (answer.checked) {
+      userAnswer = answer.value;
+    }
+  });
+  if (userAnswer == correctAnswer) {
+    textStatus.textContent = "Correct! Well done.";
+  } else {
+    textStatus.textContent = "incorrect. Try again!";
+  }
+}
+const submitButton = document.getElementById("submit-answer");
+submitButton.addEventListener("click", checkAnswer);
